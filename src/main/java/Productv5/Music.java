00ceda230334cae3e62e57d7fe9763c8dd1d049e
Tyrese <g00421432@atu.ie>
2024-01-31 17:06:09 +0000

    Added new Code

---
+++ src/main/java/Productv5/Music.java
@@ -0,0 +1,31 @@
+package Productv5;
+
+import ie.atu.Productv5.Product;
+
+public class Music extends Product {
+    private String Artist;
+    private String Label;
+    public Music() {
+        super();
+        Artist = "";
+        Label = "";
+        count++;
+    }
+
+    public void setLabel(String label) { Label = label;}
+
+    public String getLabel() {return Label;}
+
+    public void setArtist(String artist) {
+        this.Artist = artist;
+    }
+
+    public String getArtist() {
+        return Artist;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + Artist;
+    }
+}
---
+++ src/main/java/Productv5/ProductDB.java
@@ -0,0 +1,15 @@
+package Productv5;
+
+import ie.atu.Productv5.Book;
+
+public class ProductDB {
+    ie.atu.productv2.Book myBook = null;
+        if (productCode.equalsIgnoreCase("PINK")) {
+        myBook = new ie.atu.productv2.Book();
+        myBook.setCode(productCode);
+        myBook.setDescription("Wish you were her by Pink Floyd");
+        myBook.setPrice(8.00);
+        myBook.setArtist("Pink Floyd");
+    }
+}
+
